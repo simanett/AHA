142f8eff3cabb60716691c94a0f5d87393fb6ce6
simonicsanett <simonicsanett@simonics-air>
2015-10-19 21:18:15 +0100

    Flight osztály hozzáadása

---
+++ src/com/aha/businesslogic/model/Flight.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aha.businesslogic.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author simonicsanett
+ */
+public class Flight {
+    
+    private int flightNumber;
+    private Date departure;
+    private int flightDuration;
+    private List<Seat> seat;
+    private Airplane airplane;
+    private Airport airportTo;
+    private Airport airportFrom;
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public Date getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Date departure) {
+        this.departure = departure;
+    }
+
+    public int getFlightDuration() {
+        return flightDuration;
+    }
+
+    public void setFlightDuration(int flightDuration) {
+        this.flightDuration = flightDuration;
+    }
+
+    public List<Seat> getSeat() {
+        return seat;
+    }
+
+    public void setSeat(List<Seat> seat) {
+        this.seat = seat;
+    }
+
+    public Airplane getAirplane() {
+        return airplane;
+    }
+
+    public void setAirplane(Airplane airplane) {
+        this.airplane = airplane;
+    }
+
+    public Airport getAirportTo() {
+        return airportTo;
+    }
+
+    public void setAirportTo(Airport airportTo) {
+        this.airportTo = airportTo;
+    }
+
+    public Airport getAirportFrom() {
+        return airportFrom;
+    }
+
+    public void setAirportFrom(Airport airportFrom) {
+        this.airportFrom = airportFrom;
+    }
+    
+}
