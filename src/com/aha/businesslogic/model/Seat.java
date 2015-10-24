d1b9aa7e51eaebdef62a75d43d248ce029d4639d
Puce <Puce@192.168.0.197>
2015-10-24 17:59:52 +0200

    @Xml annotations added. seatId property added and set as @XmlID (referenced from Airplane.java and Flight.java)

--- src/com/aha/businesslogic/model/Seat.java
+++ src/com/aha/businesslogic/model/Seat.java
@@ -5,25 +5,43 @@
  */
 package com.aha.businesslogic.model;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author simonicsanett
  */
+@XmlRootElement
 public class Seat {
     
     private int row;
     private String letter;
     
+    @XmlID
+    @XmlElement
+    private String seatId;
     private Booking booking;
     
+    public void setSeatId() {
+        this.seatId = this.row + this.letter;
+    }
+    
+    @XmlElement
     public void setRow(int row) {
         this.row = row;
     }
 
+    @XmlElement
     public void setLetter(String letter) {
         this.letter = letter;
+    }
+    
+    @XmlTransient
+    public void setBooking(Booking booking) {
+        this.booking = booking;
     }
 
     public int getRow() {
@@ -34,13 +52,14 @@ public class Seat {
         return letter;
     }
 
+    public String getSeatId() {
+        return seatId;
+    }
+    
     public Booking getBooking() {
         return booking;
     }
 
-    @XmlTransient
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
+   
     
 }
