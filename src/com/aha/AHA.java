3802327d0cc07b080dcab4c0f2d28f8dcd7f2029
Puce <Puce@192.168.0.197>
2015-10-24 18:13:39 +0200

    Flight added to xml changed. Users added to xml file.

--- src/com/aha/AHA.java
+++ src/com/aha/AHA.java
@@ -6,10 +6,16 @@
 package com.aha;
 
 /*Hi*/
+import com.aha.businesslogic.model.Administrator;
 import com.aha.businesslogic.model.Airport;
 import com.aha.businesslogic.model.Flight;
+import com.aha.businesslogic.model.Operator;
+import com.aha.businesslogic.model.Passenger;
+import com.aha.businesslogic.model.User;
 import com.aha.data.AirportRepository;
 import com.aha.data.FileSystemManager;
+import com.aha.data.FlightRepository;
+import com.aha.data.UserRepository;
 import java.util.Date;
 import java.util.List;
 
@@ -23,6 +29,9 @@ public class AHA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Adding airports
+        
         AirportRepository repo = new AirportRepository();
         List<Airport> airports = repo.getAirports();
 
@@ -43,13 +52,55 @@ public class AHA {
             airports.add(dublin);
         }
 
-        Flight dubBud = new Flight();
-        dubBud.setAirportFrom(budapest);
-        dubBud.setAirportTo(dublin);
-        dubBud.setDeparture(new Date());
+        //Adding flights
+        
+        FlightRepository repoFlight = new FlightRepository();
+        List<Flight> flights = repoFlight.getFlights();
+        
+        Flight dubBud1252 = repoFlight.getFlightByFlightNumber(1252);
+        
+        if (dubBud1252 == null) {
+            dubBud1252 = new Flight();
+            dubBud1252.setFlightNumber(1252);
+            dubBud1252.setAirportFrom(budapest);
+            dubBud1252.setAirportTo(dublin);
+            dubBud1252.setDeparture(new Date());
+            flights.add(dubBud1252);
+        }
 
-        List<Flight> flights = FileSystemManager.getInstance().getState().getFlights();
-        flights.add(dubBud);
+        //Adding users
+        
+        UserRepository userRepo = new UserRepository();
+        List<User> users = userRepo.getUsers();
+        
+        Operator anett = (Operator) userRepo.getUserById(101);
+        Administrator helga = (Administrator) userRepo.getUserById(102);
+        Passenger tomi = (Passenger) userRepo.getUserById(103);
+        
+        if (anett == null) {
+            anett = new Operator();
+            anett.setId(101);
+            anett.setName("Anett");
+            anett.setEmail("anett@aha.com");
+            users.add(anett);
+        }
+        
+        if (helga == null) {
+            helga = new Administrator();
+            helga.setId(102);
+            helga.setName("helga");
+            helga.setEmail("helga@aha.com");
+            users.add(helga);
+        }
+        
+        if (tomi == null) {
+            tomi = new Passenger();
+            tomi.setId(103);
+            tomi.setName("Tomi");
+            tomi.setEmail("tomi@notaha.com");
+            users.add(tomi);
+        }
+        
         FileSystemManager.getInstance().saveState();
     }
 
