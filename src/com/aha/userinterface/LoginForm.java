cdc92d10c6740665b6de8ec136dab54ad31fe54e
Puce <Puce@192.168.0.199>
2015-11-05 23:53:27 +0100

    comment törlése

--- src/com/aha/userinterface/LoginForm.java
+++ src/com/aha/userinterface/LoginForm.java
@@ -107,7 +107,6 @@ public class LoginForm extends javax.swing.JFrame {
         } else if (user instanceof Passenger) {
             Passenger passenger = (Passenger) user;
 
-            //ListFlightsForm listFlightsForm = new ListFlightsForm(passenger, flightRepository.getFlights());
             ListFlightsForm listFlightsForm = new ListFlightsForm(passenger);
             listFlightsForm.setVisible(true);
         } else if (user instanceof Operator) {
